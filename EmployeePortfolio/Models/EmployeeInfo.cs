719c9ff47acb98cace0b1ac20f4a2de96ba921eb
ravindra-rj <ravindrajaduu089@gmail.com>
2021-03-26 09:34:10 +0530

    make dropdown savable

---
+++ EmployeePortfolio/Models/EmployeeInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EmployeePortfolio.Models
+{
+    public class EmployeeInfo
+    {
+        public string employeeName;
+        public EmployeeInfo()
+        {
+            employeeName = string.Empty;
+        }
+    }
+}
